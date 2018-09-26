02fa74c302e00d70d452152198f91b9d07ca9002
avm <alexeymajor@gmail.com>
2018-09-26 13:00:45 +0300

    partial

--- websocket/bubbles/js/click.js
+++ websocket/bubbles/js/click.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const ws = new WebSocket('wss://neto-api.herokuapp.com/mouse');
+
+ws.addEventListener('open', showBubbles(ws));
+
+document.addEventListener('click', event => {
+    ws.send(JSON.stringify({
+        x: event.clientX,
+        y: event.clientY
+    }));
+});
--- websocket/counter/js/counter.js
+++ websocket/counter/js/counter.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const counts = document.querySelector('.counter');
+const errors = document.querySelector('.errors');
+
+const ws = new WebSocket('wss://neto-api.herokuapp.com/counter');
+
+ws.addEventListener('message', event => {
+    const data = JSON.parse(event.data);
+    counts.textContent = data.connections;
+    errors.textContent = data.errors;
+});
+
+window.addEventListener('beforeunload', event => {
+    ws.close(1000, 'closed')
+});
